5a99465f63e6e8b6966f6a26d618c0f43d30639e
Shaya Golafshani <s.golaff@gmail.com>
2022-06-10 11:35:52 -0600

    Update the oop file (not done yet)

--- oop.js
+++ oop.js
@@ -3,6 +3,23 @@
 // it should have a color property, that is silver by default
 // it should have a method called "say" that returns whatever string is passed in, with "*~*" at the beginning and end of the string
 
+class Unicorn {
+  constructor(name) {
+    this.name = name;
+    this.color = "silver";
+    this.message = "hello"
+  }
+
+  Say(newMessage) {
+    this.message = `*~* ${newMessage} *~*`;
+}
+}
+
+var unicorn1 = new Unicorn("Billy");
+console.log(unicorn1);
+
+unicorn1.Say("My day is going well!");
+console.log(unicorn1);
 
 
 // Write a class called Vampire
@@ -11,7 +28,26 @@
 // it should have a thirsty property, that is true by default
 // it should have a drink method. When called, the thirsty property changes to false
 
+class Vampire {
+  constructor(name) {
+    this.name = name;
+    this.pet = "bat";
+    this.thirsty = true
+  }
 
+  changePet(newPet) {
+    this.pet = newPet;
+  }
+  drink(blood) {
+    this.thirsty = false;
+  }
+}
+
+var vampire1 = new Vampire("Dracula");
+console.log(vampire1);
+
+vampire1.drink();
+console.log(vampire1);
 
 //  Write a Dragon class
 // it should have a dynamic name property (string)
@@ -20,7 +56,26 @@
 //  it should have a isHungry property that is true by default
 //  it should have a eat method. If the dragon eats 4 times, it is no longer hungry
   // HINT: This requires you to use your knowledge from the conditionals lesson
+class Dragon {
+  constructor(name, rider, color) {
+    this.name = name;
+    this.rider = rider;
+    this.color = color;
+    this.isHungry = true;
+  }
 
+  eat(hungerStatus) {
+    if (hungerStatus >= 4) {
+    this.isHungry = false;
+  }
+}
+}
+
+var dragon1 = new Dragon("Aragon", "Tom", "blue");
+console.log(dragon1);
+
+dragon1.eat(4);
+console.log(dragon1);
 
 
 //  BIG CHALLNEGE: Write a Hobbit class
